95ae7407fd65525ddf146f70b6f86747dd828076
Thangbodoiqua <dluy2406@gmail.com>
2024-11-11 23:05:11 +0700

    Down and Add SrcCode

---
+++ src/main/java/com/spring/core/SrcCode/assignment/DataSource.java
@@ -0,0 +1,5 @@
+package com.spring.core.SrcCode.assignment;
+
+public interface DataSource {
+    String[] getEmails();
+}
---
+++ src/main/java/com/spring/core/SrcCode/di/MessageService.java
@@ -0,0 +1,5 @@
+package com.spring.core.SrcCode.di;
+
+public interface MessageService {
+    void sendMessage(String message);
+}
